4b4aac79cc35f6fd41a61e57e743be1cb1dc04bf
github username <github email address>
2018-06-09 19:59:10 -0500

    Add auth API helplers and specs

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -7,9 +7,9 @@ module ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
-    rescue_from ExceptionHandler::AuthenticationError, with :unauthorized_request
-    rescue_from ExceptionHandler::MissingToken, with :four_twenty_two
-    rescue_from ExceptionHandler::InvalidToken, with :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
 
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,5 +1,7 @@
 class JsonWebToken
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
+  # HMAC_SECRET = Rails.application.secrets.secret_key_base
+  #??? secret_key_base= no secrets.yml in 5.2
+  HMAC_SECRET = '15e0cb8f5758656ff750129c'
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
