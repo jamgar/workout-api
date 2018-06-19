6e401ee3ff16b5d52cbda77abc27009ce7fb2896
github username <github email address>
2018-06-19 10:52:42 -0500

    Add uniqueness validation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_many :workouts, foreign_key: :created_by
 
   validates_presence_of :name, :email, :password_digest
+  validates :email, uniqueness: { case_sensitive: false }
 end
